db664f75b1f5abba0a4a1bd25fb940e12ec8e9d3
sukrit007 <sukrit007@gmail.com>
2014-09-04 03:34:35 -0700

    Fix multi threading issues

--- discover/main.py
+++ discover/main.py
@@ -6,7 +6,6 @@ import os
 import yoda
 import logging
 import json
-import time
 import datetime
 
 
@@ -17,7 +16,7 @@ from apscheduler.schedulers.blocking import BlockingScheduler
 __all__ = ['scheduler']
 
 executors = {
-    'threadpool': ThreadPoolExecutor(1),
+    'default': ThreadPoolExecutor(1),
     'processpool': ProcessPoolExecutor(3)
 }
 
@@ -34,25 +33,25 @@ PROXY_HOST = os.environ.get('PROXY_HOST', '172.17.42.1')
 
 LOG_FORMAT = '%(asctime)s [%(name)s] %(levelname)s %(message)s'
 LOG_DATE = '%Y-%m-%d %I:%M:%S %p'
-DOCKER_POLL_RESTART_SECONDS=10
-EVENT_JOB_ID_FILE = '/tmp/yoda-discover-poll.id'
+DOCKER_POLL_RESTART_SECONDS=60
 
 logging.basicConfig(format=LOG_FORMAT, datefmt=LOG_DATE, level=logging.WARN)
-
 logger = logging.getLogger('yoda-discover')
 logger.level = logging.INFO
 
+scheduler = BlockingScheduler(executors=executors, job_defaults=job_defaults)
 
 
-scheduler = None
+def docker_client():
+    return docker.Client(
+        base_url=DOCKER_URL,
+        version='1.12',
+        timeout=10)
 
-docker_cl = docker.Client(
-    base_url=DOCKER_URL,
-    version='1.12',
-    timeout=10)
 
-yoda_cl = yoda.Client(etcd_host=ETCD_HOST, etcd_port=ETCD_PORT,
-                      etcd_base=ETCD_BASE)
+def yoda_client():
+    return yoda.Client(etcd_host=ETCD_HOST, etcd_port=ETCD_PORT,
+                       etcd_base=ETCD_BASE)
 
 
 def parse_container_env(env_cfg):
@@ -62,9 +61,10 @@ def parse_container_env(env_cfg):
         parsed_env[env_name] = env_value
     return parsed_env
 
-def handle_discover(container_id):
-    container_info = docker_cl.inspect_container(container_id)
 
+def handle_discover(container_id):
+    docker_cl = docker_client()
+    container_info = docker_cl.inspect_container(container_id)
     if 'Config' in container_info and 'Env' in container_info['Config']:
         env_cfg = container_info['Config']['Env']
         parsed_env = parse_container_env(env_cfg)
@@ -83,48 +83,39 @@ def handle_discover(container_id):
                                        private_port)[0]['HostPort']
                     endpoint = yoda.as_endpoint(PROXY_HOST, public_port)
                     logger.info('Publishing %s : %s', node, endpoint)
-                    yoda_cl.discover_node(upstream, node, endpoint)
+                    yoda_client().discover_node(upstream, node, endpoint)
                 else:
                     logger.info('Removing node %s : %s', upstream, node)
-                    yoda_cl.remove_node(upstream, node)
+                    yoda_client().remove_node(upstream, node)
 
 
-def handle_poll_event(event):
-    event_dict = json.loads(event)
-    scheduler.add_job(handle_discover, args=[event_dict['id']],
-                      executor='processpool')
+def schedule_discover_job(container_id):
+    logger.info('Scheduling discover job for container: %s', container_id)
+    scheduler.add_job(handle_discover, args=[container_id],
+                      executor='processpool',
+                      misfire_grace_time=300,
+                      next_run_time=datetime.datetime.now())
 
 
 
 def docker_instances_poll():
-    for container in docker_cl.containers(all=True):
-        scheduler.add_job(handle_discover, args=[container['Id']],
-                          executor='processpool')
+    for container in docker_client().containers():
+        schedule_discover_job(container['Id'])
 
+
+#Adding as a scheduled job so that it will automatically restart if docker
+#polling gets interrupted
+@scheduler.scheduled_job(
+    trigger='cron', minute='*/1', next_run_time=datetime.datetime.now(),
+    misfire_grace_time=5, executor='default')
 def docker_event_poll():
-    while(True):
-        try:
-            #Poll for all instances for first run (before event polling)
-            docker_instances_poll()
-            logger.info('Start polling for docker events....')
-            for event in docker_cl.events():
-                logger.info('%s', event)
-                handle_poll_event(event)
-
-        except:
-            logger.exception(
-                'Error happened while polling for docker events. Restarting '
-                'polling in %ds' % DOCKER_POLL_RESTART_SECONDS)
-            time.sleep(DOCKER_POLL_RESTART_SECONDS)
-
+    #Poll for all instances for first run (before event polling)
+    docker_instances_poll()
+    logger.info('Start polling for docker events....')
+    for event in docker_client().events():
+        logger.info('%s', event)
+        event_dict = json.loads(event)
+        schedule_discover_job(event_dict['id'])
 
 if __name__ == "__main__":
-    scheduler = BlockingScheduler(executors=executors,
-                                  job_defaults=job_defaults)
-    #Instance Polling to capture an
-    #scheduler.add_job(docker_instances_poll, trigger='cron', hour='*/1')
-    #Run it as cron schedule (So that it restarts if it dies for any reason)
-    scheduler.add_job(docker_event_poll, trigger='cron', minute='*/15',
-                      next_run_time=datetime.datetime.now(),
-                      executor='threadpool')
     scheduler.start()
