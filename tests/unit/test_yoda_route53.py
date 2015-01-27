3c0545aafd67674cf187e441cd15d3c7fb90c3e0
sukrit007 <sukrit007@gmail.com>
2015-01-27 03:51:39 -0700

    route53 sync tests

---
+++ tests/unit/test_yoda_route53.py
@@ -0,0 +1,28 @@
+from collections import namedtuple
+from mock import patch, MagicMock
+from nose.tools import eq_
+from discover.yoda_route53.__main__ import route53_sync
+
+Args = namedtuple('Args', 'etcd_host etcd_port etcd_base')
+
+
+def create_mock_args(etcd_host='mockhost', etcd_port=4001, etcd_base='/'):
+    return Args(etcd_host=etcd_host, etcd_port=etcd_port, etcd_base=etcd_base)
+
+
+@patch('boto.connect_route53')
+@patch('yoda.Client')
+@patch('etcd.Client')
+def test_route53_sync_with_no_polling(m_etcd_cl, m_yoda_cl, m_route53):
+    # Given: Polling function that returns false
+    should_poll = lambda: False
+
+    # And: Mock parsed arguments
+    parsed_args = create_mock_args()
+
+    # When: I perform route53 sync
+    route53_sync(parsed_args, should_poll=should_poll)
+
+    # Then: No sync is performed
+    eq_(m_route53.called, False)
+    eq_(m_yoda_cl.called, False)
